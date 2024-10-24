8ae02f8e34dff1316758afbbe64982cbeda0a01f
JellyTumTum <cameronst12345@gmail.com>
2024-10-24 16:19:42 +0100

    Had to ajdust app.ts to fix wierd server issue around listen options

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,16 @@
+import { FastifyListenOptions } from "fastify";
 import { server } from "./server"
 
-server
-  .listen(process.env.PORT || 3001, "0.0.0.0")
-  .then(() =>
-    console.log("Server running on port " + (process.env.PORT || 3001))
-  )
-  .catch((error) => {
-    console.log(error.message);
-  });
+const listenOptions: FastifyListenOptions = {
+    port: parseInt(process.env.PORT || "3001", 10),
+    host: "0.0.0.0",
+  };
+  
+  server
+    .listen(listenOptions) 
+    .then((address) => {
+      console.log(`Server running at ${address}`);
+    })
+    .catch((error: Error) => {
+      console.error(error.message);
+    });
